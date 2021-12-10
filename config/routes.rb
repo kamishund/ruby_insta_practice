ba97572e68bb485328b9acf22cad5c7d529bec10
kamishund <kamishund@gmail.com>
2021-12-11 00:51:31 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, # ここの行にカンマを追加
+  controllers: { registrations: 'registrations' } # ここの行を追加
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
-  
+
+  resources :posts, only: %i(new create index) do
+    resources :photos, only: %i(create)
+  end
+
 end
 
 
